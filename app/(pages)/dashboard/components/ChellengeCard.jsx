3814d136437b130b94eeca2ebcb0d6f011261c20
Mark Slorach <hello@markslorach.com>
2023-12-01 18:38:51 +0000

    deploy test

--- app/(pages)/dashboard/components/ChellengeCard.jsx
+++ app/(pages)/dashboard/components/ChellengeCard.jsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function ChellengeCard({difficulty}) {
+export default function ChellengeCard({ difficulty }) {
   return (
-    <Link href={`/${difficulty.toLowerCase()}`}>
-    <button className="flex flex-col w-1/2 items-center gap-3 py-4 px-3 rounded-lg bg-[#1c375c] shadow-md">
-      <div className="avatar">
-        <div className="w-14 rounded-full">
-          <Image src="/images/python_logo.png" width={100} height={100} />
+    <Link className="w-1/2 flex" href={`/${difficulty.toLowerCase()}`}>
+      <button className="flex flex-col items-center gap-3 py-4 px-3 rounded-lg bg-[#1c375c] shadow-md">
+        <div className="avatar">
+          <div className="w-14 rounded-full">
+            <Image src="/images/python_logo.png" width={256} height={256} />
+          </div>
         </div>
-      </div>
-      <h3>Python</h3>
-      <small className="text-center tracking-wide text-white/50">Challenge yourself with today's Python quiz.</small>
-    </button>
-  </Link>
-  )
+        <h3>Python</h3>
+        <small className="text-center tracking-wide text-white/50">
+          Challenge yourself with today's Python quiz.
+        </small>
+      </button>
+    </Link>
+  );
 }
