5df019e69254dde47cb85386acd6d2e12e5a55b6
Mark Slorach <hello@markslorach.com>
2023-12-01 14:11:05 +0000

    challenge card update

--- app/(pages)/dashboard/components/ChellengeCard.jsx
+++ app/(pages)/dashboard/components/ChellengeCard.jsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function ChellengeCard({difficulty}) {
   return (
     <Link href={`/${difficulty.toLowerCase()}`}>
-    <button className="flex flex-col w-1/2 items-center gap-3 p-3 rounded-lg bg-[#1c375c] shadow-md border-4 border-white/50">
+    <button className="flex flex-col w-1/2 items-center gap-3 py-4 px-3 rounded-lg bg-[#1c375c] shadow-md">
       <div className="avatar">
-        <div className="w-20 rounded-full">
-          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="w-14 rounded-full">
+          <Image src="/images/python_logo.png" width={100} height={100} />
         </div>
       </div>
       <h3>Python</h3>
