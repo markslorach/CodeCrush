68fe7c65aae4280fc5281b14c853fe7bf823e010
Mark Slorach <hello@markslorach.com>
2023-12-02 12:13:30 +0000

    defaultChecked

--- app/(game)/python/beginner/page.jsx
+++ app/(game)/python/beginner/page.jsx
@@ -10,6 +10,7 @@ export default function Beginner() {
             type="radio"
             name="radio-10"
             className="radio checked:bg-blue-500"
+            defaultChecked
           />
         </label>
       </div>
