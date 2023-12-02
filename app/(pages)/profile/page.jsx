ccf461d7ec6a9349bc040e291bb676a17a329046
Mark Slorach <hello@markslorach.com>
2023-12-02 00:51:40 +0000

    tailwind focus mobile test

--- app/(pages)/profile/page.jsx
+++ app/(pages)/profile/page.jsx
@@ -15,6 +15,9 @@ export default function Profile() {
     <div>
       Profile
       <SignOutButton />
+      <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+  Save changes
+</button>
     </div>
   );
 }
